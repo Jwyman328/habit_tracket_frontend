f1ea7c1f8646464b2f5e8302d2bcc2740c69ad80
JosephWyman328 <Jwyman@student.framingham.edu>
2019-12-27 12:48:55 -0500

    removed mode of no cors

--- src/activity_components/CreateActivity.js
+++ src/activity_components/CreateActivity.js
@@ -102,7 +102,7 @@ function CreateActivity(props) {
         let post_data_formated = {habit_id:habitData.id, start_time:now, end_time:end_time_value, total_time: null} //record time it was done 
         let postDataJson = JSON.stringify(post_data_formated)
         let token = localStorage.getItem('token')
-        let getResponse = await fetch('http://shrouded-ravine-06737.herokuapp.com/habits/create_activity',{method:'POST', mode: 'no-cors',body:postDataJson,
+        let getResponse = await fetch('http://shrouded-ravine-06737.herokuapp.com/habits/create_activity',{method:'POST', body:postDataJson,
         headers:{ Authorization: `JWT ${token}`,'Content-Type': 'application/json'}}
         );
         let jsonResponse = await getResponse.json()
@@ -131,7 +131,7 @@ function CreateActivity(props) {
         //let post_data_formated = {habit_id:habitData.id, start_time:now, end_time:end_time_value, total_time: null} //record time it was done 
         //let postDataJson = JSON.stringify(post_data_formated)
         let token = localStorage.getItem('token')
-        let putResponse = await fetch(`http://shrouded-ravine-06737.herokuapp.com/habits/activities/update/${activity_id}/${year}/${month}/${day}/${hours}/${minutes}/${seconds}/`,{method:'PUT', mode: 'no-cors', //body:postDataJson
+        let putResponse = await fetch(`http://shrouded-ravine-06737.herokuapp.com/habits/activities/update/${activity_id}/${year}/${month}/${day}/${hours}/${minutes}/${seconds}/`,{method:'PUT',  //body:postDataJson
         headers:{ Authorization: `JWT ${token}`,'Content-Type': 'application/json'}});
         
         let jsonResponse = await putResponse.json()
