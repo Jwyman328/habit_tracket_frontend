af4ce2341c9ed1c97bdb5f9d9211048885152eba
Joseph Wyman <jwymandev@gmail.com>
2020-05-12 10:49:31 -0400

    Remove unused file

--- src/utils/manipulateDataHelperFunctions/handleStartDateChange.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-    let handleStartDateChange = (date, formData) => {
-        let YYYYMMDD_date = formatDate(date)
-        let newFormData = {...formData}
-        newFormData['start_date'] = YYYYMMDD_date
-        setFormData(newFormData)
-        setStartDate(date)
-    }
